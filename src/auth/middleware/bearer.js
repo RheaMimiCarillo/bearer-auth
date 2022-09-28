e495f15a665a86f74746790e5cbe8aca917d19f2
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-09-28 10:34:42 -0700

    added proposed file structure

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -19,3 +19,46 @@ module.exports = async (req, res, next) => {
     res.status(403).send('Invalid Login');
   }
 }
+
+/*
+// look at req.header bearer bits and look at the JWT
+
+// return something that says if the JWT is a valid token or not
+
+'use strict'
+
+const Users = require('./models/Users');
+
+// is this redundant?
+const { Users } = require('./models/Users.js');
+
+async function Bearer(req, res, next)
+{
+
+  // ['Bearer', JWT-string-thing]
+  // remove the first thing in the header (the bearer)
+  // and then just save the `JWT` as the `token` variable
+  let authHeader = req.headers.authorization.split(' ')
+  let token = authHeader[1];
+
+  try
+  {
+    const user = await Users.authenticateBearer(token);
+    if (user)
+    {
+      req.user = user;
+      next();
+    }
+    else
+    {
+      next('invalid token');
+    }
+  }
+  catch (error)
+  {
+    next(error);
+  }
+}
+
+
+*/
--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -1,45 +1,77 @@
 'use strict';
 
+// we're never saving plain text!
+
+const { Sequelize, DataTypes } = require('sequelize');
+
 const bcrypt = require('bcrypt');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const userSchema = (sequelize, DataTypes) => {
-  const model = sequelize.define('User', {
-    username: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false, },
-    token: {
-      type: DataTypes.VIRTUAL,
-      get() {
-        return jwt.sign({ username: this.username });
-      }
+// Create a Sequelize model
+const Users = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+
+Users.beforeCreate(async (user, options) =>
+{
+  // hash our password here with difficulty 10
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashedPassword;
+});
+
+// ask our model to validate a password
+// Model, you should be able to tell us if this username and password is valid
+Users.authenticateBasic = async (username, password) =>
+{
+  try
+  {
+    const user = await Users.findOne({ where: { username: username } });
+
+    // I assume valid will return true? or something
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid)
+    {
+      return user;
     }
-  });
-
-  model.beforeCreate(async (user) => {
-    let hashedPass = bcrypt.hash(user.password, 10);
-    user.password = hashedPass;
-  });
-
-  // Basic AUTH: Validating strings (username, password) 
-  model.authenticateBasic = async function (username, password) {
-    const user = await this.findOne({ username })
-    const valid = await bcrypt.compare(password, user.password)
-    if (valid) { return user; }
-    throw new Error('Invalid User');
+  }
+  catch (error)
+  {
+    throw new Error(error);
   }
 
-  // Bearer AUTH: Validating a token
-  model.authenticateToken = async function (token) {
-    try {
-      const parsedToken = jwt.verify(token, process.env.SECRET);
-      const user = this.findOne({ username: parsedToken.username })
-      if (user) { return user; }
-      throw new Error("User Not Found");
-    } catch (e) {
-      throw new Error(e.message)
-    }
-  }
-
-  return model;
 }
 
-module.exports = userSchema;
+Users.authenticateToken = async function (token)
+{
+  // verify the integrity of our token using the SECRET in the .env
+
+  try
+  {
+    const parsedToken = jwt.verify(token, API_SECRET);
+
+    // grab the user bits from the parsed token
+    const user = await this.findOne({ where: { username: parsedToken.username } });
+  }
+  catch(error)
+  {
+    throw new Error(error);
+  }
+}
+
+// before-create hook
+// google: sequelize before-create hook
+// https://sequelize.org/docs/v6/other-topics/hooks/
+// functions that are called before 
+
+module.exports = {
+  Users,
+  // export sequelize and call it `db`
+  db: sequelize
+}
